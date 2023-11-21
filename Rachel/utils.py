9a78d856d4d80940c338e2c8de45730a74c6985b
AmirElimelech <sapinhopreto@gmail.com>
2023-11-21 19:08:50 +0200

    adding logging and try and except into Utils file

--- Rachel/utils.py
+++ Rachel/utils.py
@@ -1,70 +1,49 @@
 from django.core.mail import send_mail
 from .models import FailedLoginAttempt
-from DAL import DAL
+from .DAL import DAL
+import logging
 
 
 
 SUSPICIOUS_ATTEMPT_THRESHOLD = 5
 
 dal = DAL()
+logger = logging.getLogger(__name__)
 
 def alert_for_suspicious_activity(username):
-    attempt, created = dal.get_or_create(
-        FailedLoginAttempt,
-        defaults={'attempts': 1},
-        username=username
-    )
-
-    # Check if the account is already locked out
-    if dal.is_locked_out(attempt):
-        return
-
-    if not created:
-        attempt.attempts += 1
-        dal.update(attempt)
-
-    if attempt.attempts > SUSPICIOUS_ATTEMPT_THRESHOLD:
-        send_mail(
-            'Suspicious Login Activity Detected',
-            f'Multiple failed login attempts for user: {username}',
-            'from@example.com',
-            ['admin@example.com'],
-            fail_silently=False,
+    try:
+        attempt, created = dal.get_or_create(
+            FailedLoginAttempt,
+            defaults={'attempts': 1},
+            username=username
         )
-        dal.lock_out(attempt)
-    elif attempt.attempts == SUSPICIOUS_ATTEMPT_THRESHOLD:
-        dal.lock_out(attempt)
+
+        # Check if the account is already locked out
+        if dal.is_locked_out(attempt):
+            logger.warning(f"Suspicious activity detected for user {username}, but the account is already locked out.")
+            return
+
+        if not created:
+            attempt.attempts += 1
+            dal.update(attempt)
+
+        logger.info(f"Suspicious activity detected for user {username}. Attempts: {attempt.attempts}")
 
 
+        if attempt.attempts > SUSPICIOUS_ATTEMPT_THRESHOLD:
+            send_mail(
+                'Suspicious Login Activity Detected',
+                f'Multiple failed login attempts for user: {username}',
+                'from@example.com',
+                ['admin@example.com'],
+                fail_silently=False,
+            )
+            dal.lock_out(attempt)
+            logger.warning(f"User {username} locked out due to suspicious activity.")
 
-# def alert_for_suspicious_activity(username):
-#     # Create or update a record of the failed login attempt
-#     attempt, created = FailedLoginAttempt.objects.get_or_create(
-#         username=username,
-#         defaults={'attempts': 1},
-#     )
+        elif attempt.attempts == SUSPICIOUS_ATTEMPT_THRESHOLD:
+            dal.lock_out(attempt)
+            logger.warning(f"User {username} locked out due to reaching the suspicious attempt threshold.")
 
-#     # Check if the account is already locked out
-#     if attempt.is_locked_out():
-#         # Do not increment attempts if the account is locked out
-#         return
-
-#     if not created:
-#         attempt.attempts += 1
-#         attempt.save()
-
-#     # Check if the number of failed attempts is above the threshold
-#     if attempt.attempts > SUSPICIOUS_ATTEMPT_THRESHOLD:
-#         # Send an email alert to the admin
-#         send_mail(
-#             'Suspicious Login Activity Detected',
-#             f'Multiple failed login attempts for user: {username}',
-#             'from@example.com',
-#             ['admin@example.com'],
-#             fail_silently=False,
-#         )
-#         # Lockout the user
-#         attempt.lock_out()
-#     elif attempt.attempts == SUSPICIOUS_ATTEMPT_THRESHOLD:
-#         # Start the lockout period
-#         attempt.lock_out()
+    except Exception as e:
+        logger.exception(f"Error in alert_for_suspicious_activity: {str(e)}")
