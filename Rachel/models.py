39c0585bfb70be98ca142ed05b1b16fead64e1c4
AmirElimelech <sapinhopreto@gmail.com>
2023-11-21 18:55:01 +0200

    updated the models.py file and re arranged models by order and added comments for each model

--- Rachel/models.py
+++ Rachel/models.py
@@ -1,22 +1,37 @@
 
-from django.contrib.gis.db import models as geomodels
+# Imports 
+
 from django.db import models
-from django.contrib.auth.models import User, Group
-from django.core.validators import RegexValidator, FileExtensionValidator , MaxLengthValidator
-from django.utils.translation import gettext_lazy as _
-from django.core.exceptions import ValidationError
-from datetime import date , timedelta
 from django.utils import timezone
 from axes.models import AccessAttempt
-from fernet_fields import EncryptedCharField
+from datetime import date ,  timedelta
+from django_cryptography.fields import encrypt
 from django_countries.fields import CountryField
+from django.contrib.auth.models import User, Group
+from django.core.exceptions import  ValidationError
+from django.utils.translation import gettext_lazy as _
 from phonenumber_field.modelfields import PhoneNumberField
+from django.core.validators import FileExtensionValidator , MaxLengthValidator
 
 
 
 
 
+#Models 
+
+
+#TimestampedModel
 class TimestampedModel(models.Model):
+
+    """
+    An abstract base model that provides timestamp fields for creation, update, and deletion.
+    This model can be inherited by other models to avoid redundancy in defining these common fields.
+    Fields:
+    - created_at: The datetime when the record was created.
+    - updated_at: The datetime when the record was last updated.
+    - deleted_at: The datetime when the record was soft deleted, if applicable.
+    """
+
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     deleted_at = models.DateTimeField(null=True, blank=True)
@@ -29,6 +44,14 @@ class TimestampedModel(models.Model):
 
 # Language Model
 class Language(TimestampedModel):
+    """
+    Model representing a language. It includes the language name and a unique language code.
+    This model can be used to track different languages spoken by users or supported by the application.
+    Fields:
+    - name: The name of the language.
+    - lang_code: A unique 3-letter code representing the language.
+    """
+
     name = models.CharField(max_length=100, unique=True)
     lang_code = models.CharField(
         max_length=3,
@@ -53,9 +76,60 @@ class Language(TimestampedModel):
         ).exists()
 
 
+# Country Model
+class Country(TimestampedModel):
+
+    """
+    Basic model representing countries. It includes the country's name, its ISO code, and the phone code.
+    This model can be used for storing information related to countries, such as in address details.
+    Fields:
+    - name: The name of the country.
+    - region: The geographical or political region of the country.
+    - iso3: A unique 3-letter ISO code for the country.
+    - phone_code: The international phone code for the country.
+    """
 
 
+    name = models.CharField(max_length=100, unique=True)
+    region = models.CharField(max_length=50, blank=False, null=False)
+    iso3 = models.CharField(max_length=3, unique=True)
+    phone_code = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
+
+
+
+
+# City Model
+class City(TimestampedModel):
+
+    """
+    Model representing cities. Each city is linked to a Country and includes population data.
+    Useful for location-based information and services.
+    Fields:
+    - name: The name of the city.
+    - country: A reference to the Country model, indicating which country the city belongs to.
+    - population: The population count of the city.
+    """
+    name = models.CharField(max_length=100)
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+    population = models.BigIntegerField()
+
+    def __str__(self):
+        return f"{self.name}, {self.country.name}"
+
+#Intentions Model
 class Intentions(models.Model):
+
+    """
+    Model representing various intentions or reasons for seeking assistance.
+    Examples include seeking shelter, job opportunities, medical assistance, etc.
+    This model allows users to select multiple intentions from predefined choices.
+    Fields:
+    - intentions: A ManyToManyField to associate multiple intentions using predefined choices.
+    """
+
     INTENTION_CHOICES = [
         ('shelter', _('Seeking Shelter')),
         ('job', _('Looking for Job Opportunities')),
@@ -81,25 +155,52 @@ class Intentions(models.Model):
     ]
 
     # Use a ManyToManyField with choices
-    intentions = models.ManyToManyField('Intentions', choices=INTENTION_CHOICES)
+    intentions = models.ManyToManyField('Intentions', choices=INTENTION_CHOICES, related_name='intention_set')
 
     def __str__(self):
         return ', '.join([self.get_intentions_display() for intention in self.intentions.all()])
 
+#SupportProviderCategory Model
+class SupportProviderCategory(TimestampedModel):
+
+    """
+    Model representing categories for support providers. Categories could include
+    shelter, medical assistance, food services, etc. This helps in organizing
+    support providers into different services they offer.
+    Fields:
+    - name: The name of the category.
+    """
+
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
 
 
 
 
+#Profile Model
 class Profile(TimestampedModel):
+
+    """
+    Model representing user profiles. It includes personal information, preferences,
+    and other relevant details. It is linked to Django's User model for authentication purposes.
+    Fields include identification details, language proficiency, activity status, and more.
+    Methods:
+    - clean: Validates the profile data according to specific rules.
+    """
+
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    role = models.ForeignKey(Group, on_delete=models.CASCADE)
-    ID = models.CharField(max_length=9, unique=True)
+    roles = models.ManyToManyField(Group, blank=False)
+    identification_number = models.CharField(max_length=20, unique=True, blank=False, null=False, help_text=_("Enter your national identification number."))
+    id_type = models.CharField(max_length=30, blank=False, null=False, help_text=_("Type of identification (e.g., 'Israeli ID', 'Passport')."))
+    country_of_issue = CountryField(blank=False, null=False, default='IL',  help_text=_("Country of issue of the identification."))
     languages_spoken = models.ManyToManyField('Language', blank=True)
-    active = models.BooleanField(default=True)
+    active = models.BooleanField(default=False)
     active_until = models.DateField(null=True, blank=True)
     looking_to_earn = models.BooleanField(default=False)
     intentions = models.ManyToManyField('Intentions', blank=True)
-    address = models.CharField(max_length=200, blank=True, null=True)
+    address = encrypt(models.CharField(max_length=200, blank=True, null=True))
     profile_picture = models.ImageField(
         upload_to='profile_pictures/',
         blank=True,
@@ -109,14 +210,17 @@ class Profile(TimestampedModel):
     )
     country = CountryField(blank_label='(select country)', blank=True, null=True)
     phone_number = PhoneNumberField(blank=True, null=True, help_text=_('Enter a valid phone number'))
+    terms_accepted = models.BooleanField(default=False, verbose_name=_("Terms Accepted"))
+
+
 
     def clean(self):
         """Validate the profile data."""
         super().clean()  # Call the base class clean method
         errors = {}
 
-        if self.role.name == "SupportProvider" and not self.languages_spoken.exists():
-            errors['languages_spoken'] = _("A support provider must have at least one language spoken.")
+        if not self.languages_spoken.exists():
+            errors['languages_spoken'] = _("At least one language must be spoken.")
 
         if self.active_until and self.active_until < date.today():
             errors['active_until'] = _("The active until date cannot be in the past.")
@@ -124,8 +228,12 @@ class Profile(TimestampedModel):
         if not self.phone_number:
             errors['phone_number'] = _("A phone number is required.")
 
-        if self.role.name == "Civilian" and not self.intentions.exists():
+        if 'Civilian' in self.roles.values_list('name', flat=True) and not self.intentions.exists():
             errors['intentions'] = _("At least one intention must be selected for civilians.")
+
+        if not self.terms_accepted:
+            errors['terms_accepted'] = _("You must accept the terms to create an account.")
+
 
         if errors:
             raise ValidationError(errors)
@@ -143,26 +251,19 @@ class Profile(TimestampedModel):
             return self.phone_number.as_e164
         return None
 
-class SupportProviderCategory(TimestampedModel):
-
-    """
-    Defines categories for support providers: shelter, medical assistance, resturant , Taxi station , school  etc...
-    """
-
-    name = models.CharField(max_length=100, unique=True)
-
-    def __str__(self):
-        return self.name
 
 
-
-
-
+#SupportProvider Model
 class SupportProvider(TimestampedModel):
 
     """
-    Represents support providers, linked to a Profile and categorized by SupportProviderCategory.
-    Additional info can be provided in a text field.
+    Model representing support providers, individuals or organizations that offer
+    assistance or services. Each support provider is linked to a profile and categorized.
+    Additional information about the services provided can be included.
+    Fields:
+    - profile: A link to the user profile of the support provider.
+    - category: The category of services provided.
+    - additional_info: Additional details about the services offered.
     """
 
     profile = models.OneToOneField(Profile, on_delete=models.CASCADE)
@@ -179,10 +280,16 @@ class SupportProvider(TimestampedModel):
 class Shelters(TimestampedModel):
 
     """
-    Represents shelters with address, geolocation, capacity, and related images.
-    Each shelter is optionally linked to a SupportProvider.
-    The clean method ensures valid data for capacity and geolocation.
-    Includes properties for phone and email derived from the linked support provider.
+    Model representing shelters. Includes details like location, capacity, and related images.
+    Shelters can be optionally linked to a SupportProvider for additional contact and service information.
+    Fields:
+    - name, address, city, country: Location details of the shelter.
+    - geolocation: Geographical coordinates of the shelter.
+    - capacity: The number of individuals the shelter can accommodate.
+    - picture: Images of the shelter.
+    - support_provider: Link to a support provider if applicable.
+    Methods:
+    - clean: Validates shelter data, ensuring all necessary information is accurate and complete.
     """
 
 
@@ -190,7 +297,8 @@ class Shelters(TimestampedModel):
     address = models.CharField(max_length=200)
     city = models.ForeignKey('City', on_delete=models.SET_NULL, null=True)
     country = models.ForeignKey('Country', on_delete=models.SET_NULL, null=True)
-    geolocation = geomodels.PointField()
+    latitude = models.FloatField()  
+    longitude = models.FloatField()
     capacity = models.PositiveIntegerField()
     picture = models.ImageField(
         upload_to='shelter_pictures/',
@@ -218,8 +326,13 @@ class Shelters(TimestampedModel):
         if self.capacity <= 0:
             errors['capacity'] = _("Capacity must be a positive number.")
 
-        if not self.geolocation:
-            errors['geolocation'] = _("A shelter must have a valid geolocation.")
+        # Geolocation validation
+        if self.geolocation:
+            latitude, longitude = self.geolocation.y, self.geolocation.x
+            if not (-90 <= latitude <= 90):
+                errors['geolocation'] = _("Latitude must be between -90 and 90 degrees.")
+            if not (-180 <= longitude <= 180):
+                errors['geolocation'] += _(" Longitude must be between -180 and 180 degrees.")
 
         if self.city is None or self.country is None:
             errors['location'] = _("A shelter must be associated with a city and a country.")
@@ -247,46 +360,38 @@ class Shelters(TimestampedModel):
 
 
 
-# Country Model
-class Country(TimestampedModel):
-
-    """
-    Basic model for countries, including name, ISO code, and phone code.
-    """
-
-
-    name = models.CharField(max_length=100)
-    iso3 = models.CharField(max_length=3, unique=True)
-    phone_code = models.CharField(max_length=10)
-
-    def __str__(self):
-        return self.name
-
-
-
-
-# City Model
-class City(TimestampedModel):
-    """
-    Represents cities, each linked to a Country and includes a population field.
-    """
-    name = models.CharField(max_length=100)
-    country = models.ForeignKey(Country, on_delete=models.CASCADE)
-    population = models.BigIntegerField()
-
-    def __str__(self):
-        return f"{self.name}, {self.country.name}"
-
+#UserActivity Model
 
 class UserActivity(TimestampedModel):
 
     """
-    Tracks user activities like login, logout, etc., along with the user's IP address and timestamp of the activity.
+    Model for tracking various user activities, such as logins and logouts.
+    It includes user identification, the type of activity, and the timestamp of the activity.
+    Fields:
+    - user: Reference to the User model for identifying the user.
+    - activity_type: Type of activity performed (e.g., 'login', 'logout').
+    - ip_address: IP address from which the activity was performed.
+    - timestamp: The datetime when the activity occurred.
     """
-
+    ACTIVITY_CHOICES = [
+        ('login', _('Login')),
+        ('logout', _('Logout')),
+        ('password_change', _('Password Change')),
+        ('profile_update', _('Profile Update')),
+        ('failed_login', _('Failed Login Attempt')),
+        ('account_creation', _('Account Creation')),
+        ('account_deletion', _('Account Deletion')),
+        ('password_reset_request', _('Password Reset Request')),
+        ('tos_acceptance', _('Terms of Service Acceptance')),
+        ('session_timeout', _('Session Timeout')),
+        ('file_upload', _('File Upload')),
+        ('file_download', _('File Download')),
+        ('api_access', _('API Access')),
+        ('user_feedback', _('User Feedback Submission')),
+    ]
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    activity_type = models.CharField(max_length=10)  # e.g., 'login', 'logout'
+    activity_type = models.CharField(max_length=50, choices=ACTIVITY_CHOICES)
     ip_address = models.GenericIPAddressField()  # To store the IP address
     timestamp = models.DateTimeField(auto_now_add=True)  # Records the time of activity
 
@@ -295,13 +400,38 @@ class UserActivity(TimestampedModel):
     
 
 
+#UserFeedback Model
+class UserFeedback(TimestampedModel):
+    """
+    Model for storing user feedback.
+    Fields:
+    - user: Reference to the User model for identifying the user giving feedback.
+    - feedback_text: The actual feedback provided by the user.
+    """
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    feedback_text = models.TextField()
+
+    def __str__(self):
+        return f"{self.user.username} - Feedback at {self.created_at}"
+
+
+
+
+#FailedLoginAttempt Model
 class FailedLoginAttempt(TimestampedModel):
 
 
     """
-    Tracks failed login attempts and implements a lockout mechanism.
-    Uses the AccessAttempt model from the `axes` package.
-    Includes methods to check if an account is locked out and to initiate a lockout.
+    Model tracking failed login attempts. It is used to implement security measures like
+    account lockouts after a certain number of failed attempts. It utilizes the AccessAttempt
+    model from the 'axes' package.
+    Fields:
+    - attempt: A link to the AccessAttempt model instance.
+    - lockout_until: The datetime until which the account is locked.
+    Methods:
+    - is_locked_out: Checks if the account is currently locked out.
+    - lock_out: Initiates a lockout for a set duration.
     """
 
 
