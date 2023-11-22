0d2465aaaccce09e0a9485d78993de782dae5537
AmirElimelech <sapinhopreto@gmail.com>
2023-11-23 01:08:31 +0200

    fixed the utils.py file for robust security measures and used Axes package - tested working 100%

--- Rachel/utils.py
+++ Rachel/utils.py
@@ -1,6 +1,16 @@
 from django.core.mail import send_mail
-from .models import FailedLoginAttempt
 from .DAL import DAL
+from django.utils import timezone
+from datetime import timedelta
+from axes.models import AccessAttempt
+from django.db.models import Sum
+
+from axes.attempts import get_user_attempts
+from axes.helpers import get_client_ip_address
+
+
+
+
 import logging
 
 
@@ -10,40 +20,45 @@ SUSPICIOUS_ATTEMPT_THRESHOLD = 5
 dal = DAL()
 logger = logging.getLogger(__name__)
 
-def alert_for_suspicious_activity(username):
+
+
+def alert_for_suspicious_activity(username, request=None):
     try:
-        attempt, created = dal.get_or_create(
-            FailedLoginAttempt,
-            defaults={'attempts': 1},
-            username=username
-        )
+        # Get the client IP address from the request
+        ip_address = get_client_ip_address(request)
 
-        # Check if the account is already locked out
-        if dal.is_locked_out(attempt):
-            logger.warning(f"Suspicious activity detected for user {username}, but the account is already locked out.")
-            return
+        # Fetch all AccessAttempt instances for this IP address
+        attempts_from_ip = AccessAttempt.objects.filter(ip_address=ip_address)
 
-        if not created:
-            attempt.attempts += 1
-            dal.update(attempt)
+        total_failures = 0
+        user_failure_updated = False
 
-        logger.info(f"Suspicious activity detected for user {username}. Attempts: {attempt.attempts}")
+        # Log the number of attempts for each user from this IP and increment the count
+        for attempt in attempts_from_ip:
+            user_failures = attempt.failures_since_start
+            if attempt.username == username:
+                user_failures += 1  # Incrementing for the current user
+                user_failure_updated = True
+            logger.info(f"User {attempt.username} from IP {ip_address} has {user_failures} failed attempts")
+            total_failures += user_failures
 
+        # If the current user's failure wasn't in the list, add it manually
+        if not user_failure_updated:
+            logger.info(f"User {username} from IP {ip_address} has 1 failed attempt")
+            total_failures += 1
 
-        if attempt.attempts > SUSPICIOUS_ATTEMPT_THRESHOLD:
+        logger.info(f"Total login failures from IP {ip_address}: {total_failures}")
+
+        # Alert if the number of attempts exceeds the threshold
+        if total_failures > SUSPICIOUS_ATTEMPT_THRESHOLD:
             send_mail(
                 'Suspicious Login Activity Detected',
-                f'Multiple failed login attempts for user: {username}',
-                'from@example.com',
-                ['admin@example.com'],
+                f'Multiple failed login attempts from IP: {ip_address}',
+                'rachel.for.israel@gmail.com',
+                ['sapinhopreto@gmail.com'],
                 fail_silently=False,
             )
-            dal.lock_out(attempt)
-            logger.warning(f"User {username} locked out due to suspicious activity.")
-
-        elif attempt.attempts == SUSPICIOUS_ATTEMPT_THRESHOLD:
-            dal.lock_out(attempt)
-            logger.warning(f"User {username} locked out due to reaching the suspicious attempt threshold.")
+            logger.warning(f"Suspicious activity detected from IP {ip_address} with {total_failures} total failures.")
 
     except Exception as e:
         logger.exception(f"Error in alert_for_suspicious_activity: {str(e)}")
