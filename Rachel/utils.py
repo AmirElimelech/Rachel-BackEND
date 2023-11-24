e9e1166c67353230093ca789acf17d3be90010ff
AmirElimelech <sapinhopreto@gmail.com>
2023-11-24 03:53:51 +0200

    Improved alert for suspicious activity mail sending capabilities

--- Rachel/utils.py
+++ Rachel/utils.py
@@ -2,6 +2,7 @@ import logging
 from .DAL import DAL
 from axes.models import AccessAttempt
 from django.core.mail import send_mail
+from django.contrib.auth.models import Group
 from axes.helpers import get_client_ip_address
 
 
@@ -18,9 +19,7 @@ logger = logging.getLogger(__name__)
 
 
 
-
 def alert_for_suspicious_activity(username, request=None):
-
     """
     The alert_for_suspicious_activity function is designed to log and alert on suspicious login activity based on failed login attempts. It operates as follows:
 
@@ -28,17 +27,23 @@ def alert_for_suspicious_activity(username, request=None):
 
     2. It then queries the AccessAttempt model to fetch all failed login attempt records associated with this IP address.
 
-    3. The function iterates over these records, logging the number of failed attempts for each user. For the user associated with the current failed attempt (identified by 'username'), the failure count is incremented by one to reflect the most recent failure. This increment is necessary because the logging occurs before the current attempt is committed to the database, and thus the fetched count is one less than the actual count after the failure.
+    3. The function iterates over these records, logging the number of failed attempts for each user. For the user associated with the 
+    current failed attempt (identified by 'username'), the failure count is incremented by one to reflect the most recent failure. 
+    This increment is necessary because the logging occurs before the current attempt is committed to the database, and thus the fetched 
+    count is one less than the actual count after the failure.
 
-    4. In addition to logging individual user failures, the function also calculates the total number of failed login attempts from the specified IP address. This total includes the incremented counts, providing an up-to-date view of the overall failed attempts from that IP.
+    4. In addition to logging individual user failures, the function also calculates the total number of failed login attempts from 
+    the specified IP address. This total includes the incremented counts, providing an up-to-date view of the overall failed attempts from that IP.
 
-    5. If the total number of failures exceeds a predefined threshold (SUSPICIOUS_ATTEMPT_THRESHOLD), an alert is triggered, notifying about the suspicious login activity. This alert includes sending an email and logging a warning message.
+    5. If the total number of failures exceeds a predefined threshold (SUSPICIOUS_ATTEMPT_THRESHOLD), an alert is triggered, notifying 
+    about the suspicious login activity to the system Administrator Emails ( searches for All system Administrators ). This alert includes sending an email 
+    and logging a warning message.
 
-    This approach ensures that the counts of failed login attempts are accurate and current, reflecting the latest state immediately after a login failure, thereby offering a real-time insight into suspicious activities for security monitoring purposes.
+    This approach ensures that the counts of failed login attempts are accurate and current, reflecting the latest state immediately 
+    after a login failure, thereby offering a real-time insight into suspicious activities for security monitoring purposes.
     """
 
     # Initialize the DAL instance
-    
 
     try:
         # Get the client IP address from the request
@@ -68,14 +73,25 @@ def alert_for_suspicious_activity(username, request=None):
 
         # Alert if the number of attempts exceeds the threshold
         if total_failures > SUSPICIOUS_ATTEMPT_THRESHOLD:
-            send_mail(
-                'Suspicious Login Activity Detected',
-                f'Multiple failed login attempts from IP: {ip_address}',
-                'rachel.for.israel@gmail.com',
-                ['sapinhopreto@gmail.com'],
-                fail_silently=False,
-            )
-            logger.warning(f"Suspicious activity detected from IP {ip_address} with {total_failures} total failures.")
+            # Get all administrators' emails
+            admin_group = dal.get_by_field(Group, name='Administrator')
+            if admin_group:
+                admin_emails = [user.email for user in admin_group.user_set.all() if user.email]
+            
+
+                # Log the recipients before sending the email
+                logger.info(f"Sending email to administrators: {admin_emails}")
+
+                # Send email to all administrators
+                send_mail(
+                    'Suspicious Login Activity Detected',
+                    f'Multiple failed login attempts from IP: {ip_address}',
+                    'Rachel.for.Israel@gmail.com',
+                    admin_emails,
+                    fail_silently=False,
+                )
+                
+                logger.warning(f"Suspicious activity detected from IP {ip_address} with {total_failures} total failures.")
 
     except Exception as e:
         logger.exception(f"Error in alert_for_suspicious_activity: {str(e)}")
