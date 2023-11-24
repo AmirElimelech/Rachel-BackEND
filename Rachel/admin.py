4d398e6d3b2d72d5a241630a88b7dac1e0a92e55
AmirElimelech <sapinhopreto@gmail.com>
2023-11-24 03:54:35 +0200

    Added Models to Admin Panel

--- Rachel/admin.py
+++ Rachel/admin.py
@@ -1,3 +1,87 @@
 from django.contrib import admin
+from .models import (
+    Country,
+    City,
+    Intentions,
+    SupportProviderCategory,
+    Civilian,
+    SupportProvider,
+    Administrator,
+    Language,
+    Shelter,
+    UserActivity,
+    UserFeedback,
+    Notification,
+)
 
-# Register your models here.
+
+@admin.register(Country)
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'region', 'iso3', 'phone_code')
+    exclude = ('deleted_at',)
+
+
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    list_display = ('name', 'country', 'population')
+    exclude = ('deleted_at',)
+
+
+@admin.register(Intentions)
+class IntentionsAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    exclude = ('deleted_at',)
+
+
+@admin.register(SupportProviderCategory)
+class SupportProviderCategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    exclude = ('deleted_at',)
+
+
+@admin.register(Language)
+class LanguageAdmin(admin.ModelAdmin):
+    list_display = ('name', 'lang_code')
+    exclude = ('deleted_at',)
+
+
+@admin.register(Shelter)
+class ShelterAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address', 'city', 'country', 'latitude', 'longitude', 'capacity')
+    exclude = ('deleted_at',)
+
+
+@admin.register(UserActivity)
+class UserActivityAdmin(admin.ModelAdmin):
+    list_display = ('user', 'activity_type', 'ip_address', 'timestamp')
+    exclude = ('deleted_at',)
+
+
+@admin.register(UserFeedback)
+class UserFeedbackAdmin(admin.ModelAdmin):
+    list_display = ('user', 'feedback_text', 'created_at')
+    exclude = ('deleted_at',)
+
+
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ('recipient', 'title', 'message', 'read', 'notification_type')
+    exclude = ('deleted_at',)
+
+
+@admin.register(Civilian)
+class CivilianAdmin(admin.ModelAdmin):
+    list_display = ('user', 'identification_number', 'id_type', 'country_of_issue', 'active_until', 'city', 'country', 'phone_number', 'terms_accepted')
+    exclude = ('deleted_at',)
+
+
+@admin.register(SupportProvider)
+class SupportProviderAdmin(admin.ModelAdmin):
+    list_display = ('user', 'identification_number', 'id_type', 'country_of_issue', 'active_until', 'city', 'country', 'phone_number', 'terms_accepted')
+    exclude = ('deleted_at',)
+
+
+@admin.register(Administrator)
+class AdministratorAdmin(admin.ModelAdmin):
+    list_display = ('user', 'identification_number', 'id_type', 'country_of_issue', 'active_until', 'city', 'country', 'phone_number', 'terms_accepted', 'department')
+    exclude = ('deleted_at',)
