63d4b09a3de18b36b89664584b31150843cd75b1
AmirElimelech <sapinhopreto@gmail.com>
2023-12-02 00:54:23 +0200

    Added a new method update_shelter that handles updating the shelter of a logged in supportprovider

--- Facades/support_provider_facade.py
+++ Facades/support_provider_facade.py
@@ -2,8 +2,7 @@
 import logging
 from Rachel.DAL import DAL
 from .base_facade import BaseFacade
-from Rachel.models import User, SupportProvider, FeedbackResponse, UserFeedback,UserActivity
-
+from Rachel.models import User, SupportProvider, FeedbackResponse, UserFeedback,UserActivity,Shelter
 
 
 
@@ -144,3 +143,47 @@ class SupportProviderFacade(BaseFacade):
         """
         
         return self.get_user_notifications(user_id)
+    
+
+
+
+    def update_shelter(self, support_provider_id, shelter_id, shelter_data, request):
+        """
+        Updates the details of a shelter associated with a support provider.
+
+        Args:
+            support_provider_id (int): The ID of the support provider making the update.
+            shelter_id (int): The ID of the shelter to be updated.
+            shelter_data (dict): A dictionary containing the updated shelter details.
+            request: The HTTP request object for getting the IP address.
+
+        Returns:
+            bool: True if the shelter update is successful, False otherwise.
+
+        Raises:
+            Exception: If an unexpected error occurs during the update.
+        """
+        try:
+            support_provider = self.dal.get_by_id(SupportProvider, support_provider_id)
+            shelter = self.dal.get_by_id(Shelter, shelter_id)
+
+            # Check if the shelter is associated with the support provider
+            if shelter.support_provider != support_provider:
+                logger.warning(f"Support Provider {support_provider_id} not authorized to update shelter {shelter_id}.")
+                return False
+
+            # Update the shelter details
+            updated_shelter = self.dal.update(shelter, **shelter_data)
+
+            if updated_shelter:
+                # Log the activity
+                user_ip = request.META.get('REMOTE_ADDR', '0.0.0.0')
+                self.dal.create(UserActivity, user=support_provider.user, activity_type='shelter_updated', ip_address=user_ip)
+                logger.info(f"Shelter {shelter_id} updated successfully by Support Provider {support_provider_id}.")
+                return True
+
+            return False
+
+        except Exception as e:
+            logger.error(f"Error updating shelter for Support Provider {support_provider_id}: {e}", exc_info=True)
+            return False
