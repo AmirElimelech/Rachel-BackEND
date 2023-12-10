5511049d477a7a7a0a99a669fedd07ae53f2582e
AmirElimelech <sapinhopreto@gmail.com>
2023-12-10 07:43:34 +0200

    Fixed the urls.py paths

--- ProjectRachel/urls.py
+++ ProjectRachel/urls.py
@@ -14,14 +14,26 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path , include
-from django.conf import settings
+from django.conf.urls.static import static
+
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('Rachel.urls')),
     path('Facades/', include('Facades.urls')),
+    path('Forms/', include('Forms.urls')),
+    path('Api/', include('Api.urls')),
 ]
 
+# Serve media files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# Serve static files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- Rachel/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-# # from.view import ... 
-
-
-
-urlpatterns = [
-
-
-]
