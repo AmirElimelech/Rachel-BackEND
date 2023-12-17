bd1b29f64c753bea1c26efb0e99ca8d4959cd8f4
AmirElimelech <sapinhopreto@gmail.com>
2023-12-17 20:24:24 +0200

    Added more email lines into the reset_password method to be able to notify user if he did not perform the action to contact sys admins

--- Facades/anonymous_facade.py
+++ Facades/anonymous_facade.py
@@ -2,7 +2,6 @@
 
 import logging
 from Rachel.DAL import DAL
-from django.urls import reverse
 from django.core.mail  import  send_mail
 from django.contrib.auth.models import User 
 from django.utils.encoding import force_bytes
